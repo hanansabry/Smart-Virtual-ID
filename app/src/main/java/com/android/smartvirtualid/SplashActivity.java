cbe546cdc31de697afb5d7a0cf6641c2bb7352f3
hanansabry <tweety624>
2023-02-28 01:18:43 +0200

    Initial commit

---
+++ app/src/main/java/com/android/smartvirtualid/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.android.smartvirtualid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final long SPLASH_TIME_OUT = 500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(this, MainActivity.class));
+        }, SPLASH_TIME_OUT);
+    }
+}
